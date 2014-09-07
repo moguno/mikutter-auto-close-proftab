8170e27878a1aa2968c243aeff8d0a023184c4cf
moguno <moguno@0kn.sakura.ne.jp>
2014-09-07 19:11:21 +0900

    私はプロフィール派です

--- mikutter-auto-close-proftab.rb
+++ mikutter-auto-close-proftab.rb
@@ -4,7 +4,7 @@ Plugin.create(:mikutter_auto_close_proftab) {
   UserConfig[:auto_close_proftab_period] ||= 1
 
   settings("プロフィールタブを閉じる") {
-    adjustment("プロファイルタブを閉じるまでの時間（分）", :auto_close_proftab_period, 1, 999)
+    adjustment("プロフィールタブを閉じるまでの時間（分）", :auto_close_proftab_period, 1, 999)
   }
 
   on_tab_created { |i_tab|
