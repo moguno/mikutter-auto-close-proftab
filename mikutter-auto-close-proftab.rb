0917603444f322bc04c8cc99a5d50733f300d5f2
moguno <moguno@0kn.sakura.ne.jp>
2014-09-07 15:36:31 +0900

    Initial commit

---
+++ mikutter-auto-close-proftab.rb
@@ -0,0 +1,19 @@
+#coding: utf-8
+
+Plugin.create(:mikutter_auto_close_proftab) {
+  UserConfig[:auto_close_proftab_period] ||= 1
+
+  settings("プロフィールタブを閉じる") {
+    adjustment("プロファイルタブを閉じるまでの時間（分）", :auto_close_proftab_period, 1, 999)
+  }
+
+  on_tab_created { |i_tab|
+    if i_tab.slug =~ /^profile-/
+      Reserver.new(UserConfig[:auto_close_proftab_period] * 60) {
+        Delayer.new {
+          i_tab.destroy
+        }
+      }
+    end
+  }
+}
